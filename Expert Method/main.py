90dee2751bd64c8fd6e6d102bee004c7392cccc4
Arkadiusz Torba <suabyak@gmail.com>
2022-10-24 23:10:19 +0200

    window with event operator

--- Expert Method/main.py
+++ Expert Method/main.py
@@ -1,3 +1,24 @@
 import pygame
 
-print("Hello World")
+pygame.display.set_mode((1240, 800))
+run = True
+
+def quit():
+    run = False
+
+operatable_events = {
+    pygame.constants.QUIT: quit,
+}
+
+
+def operate_events():
+    for event in pygame.event.get():
+        if event.type in operatable_events.keys():
+            operatable_events[event.type](event)
+        else:
+            print(event)
+
+
+
+while run:
+    operate_events()
